9afe87c78da83052e0a963cf56eb61e3b3bcb98e
Winnie Lwin <wintnandar.wnl@gmail.com>
2023-10-04 23:32:37 -0500

    Update EmailService.java
    EmailService

--- src/main/java/com/api/hotel/service/EmailService.java
+++ src/main/java/com/api/hotel/service/EmailService.java
@@ -1,8 +1,10 @@
 package com.api.hotel.service;
 
+import com.api.hotel.model.Email;
+
 public interface EmailService {
 
-	public void sendMail();
+	public String sendMail(String mail, Email email);
 
 	
 }
