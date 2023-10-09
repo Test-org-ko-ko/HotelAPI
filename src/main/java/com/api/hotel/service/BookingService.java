5ce9ed6e97a94d5ecbbb0ba6eb295527e4e9d6c4
Winnie Lwin <wintnandar.wnl@gmail.com>
2023-10-08 22:44:35 -0500

    Update BookingService.java
    updateBooking in interface

--- src/main/java/com/api/hotel/service/BookingService.java
+++ src/main/java/com/api/hotel/service/BookingService.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Service;
 
 public interface BookingService {
     public Booking createBooking(Booking bookingDetails, Room room);
+
+    public String updateBooking(Integer id, Booking bookingDetails);
 }
