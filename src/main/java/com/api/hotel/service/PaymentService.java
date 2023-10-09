a9adb6456028a7fefdd1e0ed7dad30751736513b
Winnie Lwin <wintnandar.wnl@gmail.com>
2023-10-08 22:44:57 -0500

    Update PaymentService.java
    Update payment

--- src/main/java/com/api/hotel/service/PaymentService.java
+++ src/main/java/com/api/hotel/service/PaymentService.java
@@ -8,5 +8,7 @@ public interface PaymentService {
 	public String makePayment(String reason, BigDecimal amount);
 
 	public Payment makeFinePayment(String reason, BigDecimal amount);
-	// send email via email service
+
+	public Payment updatePayment(Integer paymentId, BigDecimal amount) throws Exception;
+
 }
